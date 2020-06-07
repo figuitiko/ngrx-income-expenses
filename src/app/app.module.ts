8558effa6334242a7833e705dcfcf9dcb052777e
frank freeman <frank.freeman@014digital.com>
2020-06-07 12:53:40 -0500

    Adding first commit files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeExpensesComponent } from './income-expenses/income-expenses.component';
+import { StatisticsComponent } from './income-expenses/statistics/statistics.component';
+import { DetailComponent } from './income-expenses/detail/detail.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    IncomeExpensesComponent,
+    StatisticsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
